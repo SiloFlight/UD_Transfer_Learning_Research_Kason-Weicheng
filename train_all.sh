085fe7a71a80533dffed1a5f767f4111cfa78d2c
Silo_Flight <legomaniacny13@gmail.com>
2025-09-24 12:22:44 -0400

    Alter python executable

--- train_all.sh
+++ train_all.sh
@@ -9,6 +9,6 @@ TRAINING_CODES=(en_ewt fr_gsd es_gsd pt_gsd ur_udtb ug_udt vi_vtb fa_perdt)
 
 for CODE in "${TRAINING_CODES[@]}"; do
     echo "=== Training on $CODE ==="
-    python3 src/train.py "$CODE" "$EPOCHS" "$MAX_TRAINING_SIZE"
+    python3.11 src/train.py "$CODE" "$EPOCHS" "$MAX_TRAINING_SIZE"
     echo "=== Finished $CODE ==="
 done
