fcd0ffffe363699842544d01cd0964d6c4adf88c
Silo_Flight <legomaniacny13@gmail.com>
2025-09-24 01:08:07 -0400

    Set train_all flag

--- train_all.sh
+++ train_all.sh
@@ -2,7 +2,7 @@ source projectEnv/bin/activate
 
 #Configs
 EPOCHS=3
-MAX_TRAINING_SIZE=50
+MAX_TRAINING_SIZE=-1
 
 #Languages
 TRAINING_CODES=(en_ewt fr_gsd es_gsd pt_gsd ur_udtb ug_udt vi_vtb fa_perdt)
